b338db656df9fe7cc340089a9dbe74338f4db146
Amirul Islam <amirulislamalmamun@gmail.com>
2020-10-06 18:49:18 +0600

    Face Recognition updated

--- FacialAttendance/attendance.py
+++ FacialAttendance/attendance.py
@@ -43,7 +43,22 @@ def main():
         for encode_face, face_loc in zip(encode_frame, face_frame):
             matches = face_recognition.compare_faces(encode_list, encode_face)
             face_dis = face_recognition.face_distance(encode_list, encode_face)
-            print(face_dis)
+           #  print(face_dis)
+
+        match_index = np.argmin(face_dis)
+        
+        if matches[match_index]:
+            name = class_names[match_index].upper()
+            print(name)
+            y1, x2, y2, x1 = face_loc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+        
+        cv2.imshow('Webcam', img)
+        cv2.waitKey(1)
 
 
 if __name__ == "__main__":
