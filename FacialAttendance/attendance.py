565adc7e566ef725914603a3c985415d1a14c9a6
Amirul Islam <amirulislamalmamun@gmail.com>
2020-10-06 18:30:09 +0600

    Face Recognition updated

--- FacialAttendance/attendance.py
+++ FacialAttendance/attendance.py
@@ -4,28 +4,31 @@ import numpy as np
 import face_recognition
 
 
+path = 'image_dir'
+images = []
+class_names = []
+encode_list = []
+my_list = os.listdir(path)
+
+
 def findEncodings(images):
-    encode_list = []
     for img in images:
         print(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encode_list.append(encode)
-    return encode_list
 
 
-def main():
-    path = 'image_dir'
-    images = []
-    class_names = []
-    my_list = os.listdir(path)
-
+def init():
     for cl in my_list:
         cur_img = cv2.imread(f'{path}/{cl}')
         images.append(cur_img)
         class_names.append(os.path.splitext(cl)[0])
-        
-    encode_list = findEncodings(images)
+
+
+def main():
+    init()    
+    findEncodings(images)
     
     cap = cv2.VideoCapture(0)
 
